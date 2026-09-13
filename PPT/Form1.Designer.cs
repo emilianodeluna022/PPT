d6f7a0215c0333526f94633ebeab2daacf3fa7fd
José Emiliano de Luna Santos <emilianodeluna022@gmail.com>
2026-09-13 14:20:29 -0600

    Agruegue muchas cosas por ejemplo funcionamiento basico con tres botones

--- PPT/Form1.Designer.cs
+++ PPT/Form1.Designer.cs
@@ -28,58 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(254, 89);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(257, 148);
+            this.button1.Location = new System.Drawing.Point(365, 157);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "PIEDRA";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // button2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(372, 89);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.button2.Location = new System.Drawing.Point(365, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "PAPEL";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(365, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "TIJERA";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- PPT/Form1.cs
+++ PPT/Form1.cs
@@ -12,9 +12,79 @@ namespace PPT
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
         public Form1()
         {
             InitializeComponent();
+            
+            int jugador = 0;
+            int ia = 0;
+            bool resultado = false;
+            int jugar = 0;
+            
+            
+            //string volver;
+            button1.Tag = 1;
+            button2.Tag = 2;
+            button3.Tag = 3;
+
+            button1.Click += BotonJugada_Click;
+            button2.Click += BotonJugada_Click;
+            button3.Click += BotonJugada_Click;
+
+        }
+
+        private void JugarRonda(int jugador)
+        {
+            // AQUI SE JUEGA EL JUEGO 
+            int ia = MovimientoIA();
+            VerificarGanador(jugador, ia);
+        }
+
+        private int MovimientoIA()
+        {
+            int decisionIA = rnd.Next(1, 4); // 1 a 3 inclusive
+            return decisionIA;
+        }
+
+        private bool VerificarGanador(int jugador, int ia)
+        {
+            if (jugador == ia)
+            {
+                MessageBox.Show("EMPATE IA ESCOGIO "+ NombreJugada(ia));
+                return false;
+            }
+
+            // Casos en los que gana el jugador
+            if ((jugador == 1 && ia == 3) || (jugador == 2 && ia == 1) || (jugador == 3 && ia == 2))
+            {
+                MessageBox.Show("GANASTE IA ESCOGIO " + NombreJugada(ia));
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("PERDISTE IA ESCOGIO " + NombreJugada(ia));
+                return true;
+            }
+        }
+
+        private string NombreJugada(int valor)
+        {
+            switch (valor)
+            {
+                case 1: return "Piedra";
+                case 2: return "Papel";
+                case 3: return "Tijera";
+                default: return "Desconocido";
+            }
+        }
+
+        private void BotonJugada_Click(object sender, EventArgs e)
+        {
+            Button botonPresionado = (Button)sender;
+            int jugador = (int)botonPresionado.Tag; // 1, 2 o 3 según el botón
+
+            JugarRonda(jugador);
         }
     }
 }
